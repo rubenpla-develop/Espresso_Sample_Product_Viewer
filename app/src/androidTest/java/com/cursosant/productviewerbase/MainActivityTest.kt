d67f371f1772c6b31606ca1a7f92573e587c84a8
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-03 11:57:49 +0200

    feat: replace text with Spresso
    - Section 6/71

--- app/src/androidTest/java/com/cursosant/productviewerbase/MainActivityTest.kt
+++ app/src/androidTest/java/com/cursosant/productviewerbase/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.cursosant.productviewerbase
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -55,6 +56,19 @@ class MainActivityTest {
         onView(withId(R.id.etNewQuantity))
             .check(
                 matches(withText("2"))
+            )
+    }
+
+    @Test
+    fun setNewQuantity_subLimit_not_increases_textfield() {
+        onView(withId(R.id.etNewQuantity))
+            .perform(ViewActions.replaceText("11"))
+
+        onView(withId(R.id.ibSub)).perform(click())
+
+        onView(withId(R.id.etNewQuantity))
+            .check(
+                matches(withText("10"))
             )
     }
 }
